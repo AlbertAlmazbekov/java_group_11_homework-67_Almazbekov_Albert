22c2163446ad8d835b367dea5dc185d6f0746ae3
AlmazbekovAlbert <albertalmazbekov03@gmail.com>
2022-05-17 18:40:52 +0600

    сделал поиск по постранично

--- src/main/java/com/example/homework66/contoller/TovarController.java
+++ src/main/java/com/example/homework66/contoller/TovarController.java
@@ -3,10 +3,14 @@ package com.example.homework66.contoller;
 import com.example.homework66.entity.Tovar;
 import com.example.homework66.repository.TovarRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,9 +21,16 @@ public class TovarController {
     private final TovarRepository tovarRepository;
 
     @GetMapping
-    public String getIndex(Model model){
-        List<Tovar> tovars = tovarRepository.findAll();
-        model.addAttribute("tovars", tovars);
+    public String getIndex(Model model, Pageable pageable){
+        Page<Tovar> tovars = tovarRepository.findAll(pageable);
+        model.addAttribute("tovars", tovars.getContent());
         return "tovar";
     }
+
+//    @GetMapping("/search")
+//    public String getSearch(Model model, @RequestParam String pattern, Pageable pageable){
+//        Page<Tovar> tovars = tovarRepository.findByNameLikeIgnoreCase(pattern, pageable);
+//        model.addAttribute("tovars", tovars.getContent());
+//        return "tovar";
+//    }
 }
--- src/main/java/com/example/homework66/repository/TovarRepository.java
+++ src/main/java/com/example/homework66/repository/TovarRepository.java
@@ -2,7 +2,10 @@ package com.example.homework66.repository;
 
 
 import com.example.homework66.entity.Tovar;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TovarRepository extends JpaRepository<Tovar, Long> {
+//    Page<Tovar> findByNameLikeIgnoreCase(String pattern, Pageable pageable);
 }
